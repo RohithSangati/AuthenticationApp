28bb78f203922604761c7b3b5b0ab2cf38ed705b
Sanagti Rohith Kumar <sangatirohith@gmail.com>
2021-07-25 20:23:24 +0530

    'changed'

--- AuthenticationSystem/settings.py
+++ AuthenticationSystem/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y_cwy6+8_f8q^5frf0)3+53(dq4q2%ans@95d9=9a34!)%ye-)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
